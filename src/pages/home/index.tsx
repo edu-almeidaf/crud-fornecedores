169f9187ae2b193687d32f766f524a25b09606ea
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2024-09-20 23:07:49 -0300

    feat: add new Supplier Button at top of page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,24 +1,28 @@
 // import { EmptyList } from './components/empty-list'
 import { Card } from './components/card'
 import { FilterSupplier } from './components/filter-supplier'
-import { Main, SuppliersGrid } from './styles'
+import { Main, SuppliersList, AddSupplierButton } from './styles'
 
 export function Home() {
   return (
     <Main>
       <h1>Fornecedores</h1>
 
-      <FilterSupplier />
+      <AddSupplierButton to="/new">Novo Fornecedor</AddSupplierButton>
 
       {/* <EmptyList /> */}
-      <SuppliersGrid>
-        <Card name="Nome do Fornecedor" />
-        <Card name="Nome do Fornecedor" />
-        <Card name="Nome do Fornecedor a;lskdj lajsd lkjalskdjalksjdlasjdl lasjdlkajsdlkjasldkajsldjasldj asdjlaskjdlasjdlaks laksdjalsdjlsakjd lkjsadlkas daksdalskjdlas jd asldjaslkdj askldaslkdjas dlkjasjdlajsdlasj dlkasjdlka " />
-        <Card name="Nome do Fornecedor" />
-        <Card name="Nome do Fornecedor" />
-        <Card name="Nome do Fornecedor" />
-      </SuppliersGrid>
+
+      <div>
+        <SuppliersList>
+          <FilterSupplier />
+          <Card name="Nome do Fornecedor" />
+          <Card name="Nome do Fornecedor" />
+          <Card name="Nome do Fornecedor a;lskdj lajsd lkjalskdjalksjdlasjdl lasjdlkajsdlkjasldkajsldjasldj asdjlaskjdlasjdlaks laksdjalsdjlsakjd lkjsadlkas daksdalskjdlas jd asldjaslkdj askldaslkdjas dlkjasjdlajsdlasj dlkasjdlka " />
+          <Card name="Nome do Fornecedor" />
+          <Card name="Nome do Fornecedor" />
+          <Card name="Nome do Fornecedor" />
+        </SuppliersList>
+      </div>
     </Main>
   )
 }
--- src/pages/home/styles.ts
+++ src/pages/home/styles.ts
@@ -1,9 +1,10 @@
+import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
 export const Main = styled.main`
-  margin-top: 3.625rem;
-  height: calc(100vh - 3.625rem);
-  padding: 1rem;
+  margin-top: 4.25rem;
+  height: calc(100vh - 4.25rem);
+  padding: 2rem 1rem 1rem 1rem;
   display: flex;
   flex-direction: column;
   gap: 3rem;
@@ -13,17 +14,34 @@ export const Main = styled.main`
   }
 
   @media (min-width: 1024px) {
-    & {
-      padding-top: 2rem;
-
-      h1 {
-        font-size: 2.5rem;
-      }
+    h1 {
+      font-size: 2.5rem;
     }
   }
 `
 
-export const SuppliersGrid = styled.section`
+export const NewSupplierCard = styled.div`
+  padding: 1rem;
+  border-radius: ${({ theme }) => theme.radius.md};
+`
+
+export const AddSupplierButton = styled(Link)`
+  text-decoration: none;
+  background-color: ${({ theme }) => theme.colors.primary.default};
+  color: ${({ theme }) => theme.colors.white};
+  width: 100%;
+  padding: 1rem 8rem;
+  text-align: center;
+  align-self: flex-end;
+  border-radius: ${({ theme }) => theme.radius.md};
+
+  @media (min-width: 1024px) {
+    width: auto;
+    font-size: 1.25rem;
+  }
+`
+
+export const SuppliersList = styled.section`
   display: flex;
   flex-direction: column;
   gap: 0.5rem;
